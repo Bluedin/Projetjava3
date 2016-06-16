c4324706f71af843f21add933b1657960414ab21
Bluedin <pblueway@gmail.com>
2016-06-16 16:18:48 +0200

    Ajout des méthodes dans l'interface IModel

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -14,13 +14,13 @@ public interface IModel {
 	 *
 	 * @return the message
 	 */
-	String[][] getWorld();
+	Object getWorld();
 
 	/**
 	 * Load the message.
 	 *
 	 * @param key
-	 *          the key
+	 *            the key
 	 */
 	void loadWorld(int key);
 
@@ -30,4 +30,16 @@ public interface IModel {
 	 * @return the observable
 	 */
 	Observable getObservable();
+
+	int positionHeroX();
+
+	int positionHeroY();
+
+	boolean isPenetrable(int X, int Y);
+
+	boolean spellOrNot();
+
+	int positionSpellX();
+
+	int positionSpellY();
 }
