01e6df0a5f9c2f898078588bf1c20dd8993d7b00
Bluedin <pblueway@gmail.com>
2016-06-20 00:30:29 +0200

    Ajout des mouvements du héros avec les changements de sprites inclus

--- model/src/main/java/model/Hero.java
+++ model/src/main/java/model/Hero.java
@@ -1,17 +1,19 @@
 package model;
 
-public class Hero extends Element{
+public class Hero extends Element implements IMobile{
 
 	private boolean life;
 	private int score;
 	private Orientation orientation;
 	private Spell spell;
+	private boolean hasSpell;
 	
 	public Hero(int X, int Y) {
 		super(X, Y, "/sprite/lorann_r.png");
 		this.score = 0;
-		orientation = Orientation.RIGHT;
-		spell = new Spell(-5, -5);
+		this.orientation = Orientation.RIGHT;
+		this.spell = new Spell(-5, -5);
+		this.hasSpell = true;
 	}
 	
 	public boolean getLife(){
@@ -22,11 +24,84 @@ public class Hero extends Element{
 		return score;
 	}
 	
+	public boolean getHasSpell(){
+		return this.hasSpell;
+	}
+	
 	public void gainScore(int valeur){
 		score += valeur;
 	}
 	
 	public void shoot(){
+		
+	}
+
+	public void moveDown(Element element) {
+		element.Y ++;
+	}
+
+	public void moveLeft(Element element) {
+		element.X --;
+	}
+
+	public void moveUp(Element element) {
+		element.Y --;
+	}
+
+	public void moveRight(Element element) {
+		element.X ++;
+	}
+
+	@Override
+	public void moveGlobal(Element element, Orientation orientation) {
+		switch(orientation){
+		case DOWN:
+			moveDown(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_b.png");
+			break;
+		case LEFT:
+			moveLeft(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_l.png");
+			break;
+		case LEFT_DOWN:
+			moveLeft(element);
+			moveDown(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_bl.png");
+			break;
+		case RIGHT:
+			moveRight(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_r.png");
+			break;
+		case RIGHT_DOWN:
+			moveRight(element);
+			moveDown(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_br.png");
+			break;
+		case UP:
+			moveUp(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_u.png");
+			break;
+		case UP_LEFT:
+			moveUp(element);
+			moveLeft(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_ul.png");
+			break;
+		case UP_RIGHT:
+			moveUp(element);
+			moveRight(element);
+			this.orientation = orientation;
+			this.setSprite("/sprite/lorann_ur.png");
+			break;
+		default:
+			break;
+		}
 		
 	}
 
