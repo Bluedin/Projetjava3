d599fb59f2b29727520c0093c0efe0ee5c3bcd61
raphael <raphael@pc-raph.home>
2016-06-21 21:11:25 +0200

    javadoc purse

--- model/src/main/java/model/Gold.java
+++ model/src/main/java/model/Gold.java
@@ -5,6 +5,11 @@ public class Gold extends Element implements IDisappear, IPermeable {
 	private int valeur;
 	private Permeability permeability = Permeability.PENETRABLE;
 
+	/**
+	 * @param X
+	 * @param Y
+	 * this constructor of gold get the sprite of the purse and position of elements
+	 */
 	public Gold(int X, int Y) {
 		super(X, Y, "sprite/purse.png");
 		valeur = 100;
@@ -14,6 +19,9 @@ public class Gold extends Element implements IDisappear, IPermeable {
 
 	}
 
+	/**
+	 * this method is when coordinates of hero and purse are the same the purse disapear and get point
+	 */
 	public void disappear(Hero hero) {
 		this.X = -5;
 		this.Y = -5;
@@ -24,6 +32,9 @@ public class Gold extends Element implements IDisappear, IPermeable {
 
 	}
 	
+	/**
+	 * this is the method to get the permeability of this element
+	 */
 	public Permeability getPermeability(){
 		return this.permeability;
 	}
